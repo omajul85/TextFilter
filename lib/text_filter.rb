36e8b4dfef33d7fd6ea5f02deb6de8c35aa68371
Omar Alvarez <omajul2@hotmail.com>
2016-09-15 16:01:02 +0100

    Test: returns r-d when input string is red

---
+++ lib/text_filter.rb
@@ -0,0 +1,3 @@
+def text_filter(str)
+	"r-d"
+end
--- spec/text_filter_spec.rb
+++ spec/text_filter_spec.rb
@@ -1,3 +1,5 @@
+require "text_filter"
+
 describe "text filter" do
 	it "returns 'r-d' when input string is 'red'" do
 		expect(text_filter('red')).to eq "r-d"
